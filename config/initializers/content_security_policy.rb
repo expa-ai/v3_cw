a8886ca4862d81627ecf500ea4efdc4c0f7a022e
Yan Waipann <yanwaipann@gmail.com>
2025-09-12 20:04:11 +0630

    feat: update CSP entries

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,13 +5,13 @@ Rails.application.config.content_security_policy do |policy|
   # Add localhost:3000 for cross-site development embedding
   development_domains = Rails.env.development? ? ["http://localhost:3000"] : []
   
-  policy.default_src :self, *allowed_domains, *development_domains
-  policy.font_src    :self, :data, *allowed_domains, *development_domains
-  policy.img_src     :self, :data, *allowed_domains, *development_domains
-  policy.object_src  :none
-  policy.script_src  :self, *allowed_domains, *development_domains
-  policy.style_src   :self, *allowed_domains, *development_domains
-  policy.media_src   :self, :blob, *allowed_domains, *development_domains
+  # policy.default_src :self, *allowed_domains, *development_domains
+  # policy.font_src    :self, :data, *allowed_domains, *development_domains
+  # policy.img_src     :self, :data, *allowed_domains, *development_domains
+  # policy.object_src  :none
+  # policy.script_src  :self, *allowed_domains, *development_domains
+  # policy.style_src   :self, *allowed_domains, *development_domains
+  # policy.media_src   :self, :blob, *allowed_domains, *development_domains
   policy.frame_src   :self, *allowed_domains, *development_domains
   policy.frame_ancestors   :self, *allowed_domains, *development_domains
 end
