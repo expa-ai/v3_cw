a5ff244f6c29be14209ab3d9e91ffba48b6ef5af
Yan Waipann <yanwaipann@gmail.com>
2025-09-12 19:52:22 +0630

    feat: add permission policies for use with embed mode

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,11 +1,14 @@
-# Define an application-wide HTTP permissions policy. For further
-# information see https://developers.google.com/web/updates/2018/06/feature-policy
-#
-# Rails.application.config.permissions_policy do |f|
-#   f.camera      :none
-#   f.gyroscope   :none
-#   f.microphone  :none
-#   f.usb         :none
-#   f.fullscreen  :self
-#   f.payment     :self, "https://secure.example.com"
-# end
+Rails.application.config.permissions_policy do |f|
+  # Get allowed domains from environment variable
+  allowed_domains = ENV['CSP_ALLOWED_DOMAINS']&.split(',')&.map(&:strip) || []
+  
+  # Add localhost:3000 for cross-site development embedding
+  development_domains = Rails.env.development? ? ["http://localhost:3000"] : []
+  
+  f.camera      :self, *allowed_domains, *development_domains
+  f.gyroscope   :self, *allowed_domains, *development_domains
+  f.microphone  :self, *allowed_domains, *development_domains
+  f.usb         :self, *allowed_domains, *development_domains
+  f.fullscreen  :self, *allowed_domains, *development_domains
+  f.payment     :self, *allowed_domains, *development_domains
+end
